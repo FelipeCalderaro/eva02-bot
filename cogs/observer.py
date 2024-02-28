3c5e1b85556f0892d036238fade67adbd1783b72
Felipe <j.felipe.calderaro@gmail.com>
2024-02-28 15:47:14 -0300

    deploy script created

--- cogs/observer.py
+++ cogs/observer.py
@@ -1,13 +1,13 @@
+from discord.ext.commands.errors import MissingPermissions
 from models.member_time import (
     MemberTimeDataclass,
     member_time_dataclass_from_dict,
     member_time_dataclass_to_dict,
 )
+from typing import List, Optional, Tuple
 from datetime import datetime, timedelta
 from utils.logger import AppLogger
 from discord.ext import commands
-from typing import List, Tuple
-from pprint import pprint
 from os import path
 import numpy as np
 
@@ -24,6 +24,10 @@ class Observer(commands.Cog, AppLogger):
         name="my",
         description="Series of slash commands related to 'myself'(a.k.a you, the user)",
     )
+    control_subgroup = group.create_subgroup(
+        name="control",
+        description="Commands to control the interaction with the Roles",
+    )
 
     # List of roles
     # last equals to higher role
@@ -36,6 +40,9 @@ class Observer(commands.Cog, AppLogger):
         self.bot = bot
         self.logger.info("Starting 'Observer' Cog")
         self.member_time_list = self.read_from_json()
+        self.NO_PERMISSION_MSG = (
+            "You dont have the right permissions to use this command!"
+        )
 
     @my_subgroup.command(name="time")
     async def list_my_time(self, interaction: discord.Interaction) -> None:
@@ -94,6 +101,257 @@ class Observer(commands.Cog, AppLogger):
                 "Sorry, I could'nt find how many hours are left. Is this your first time here?",
                 ephemeral=True,
             )
+
+    @control_subgroup.command(name="when")
+    async def when_specific_role(
+        self,
+        interaction: discord.Interaction,
+        member: discord.Member,
+        role: Optional[discord.Role],
+    ) -> None:
+        await interaction.response.defer()
+        interaction.channel.typing()
+
+        current_member, _ = self.get_member_from_list(member.id)
+        if current_member is None:
+            await interaction.followup.send(
+                f"User {member.mention} is not on the tracking list yet"
+            )
+            return
+
+        current_role = self.get_member_highest_role(member)
+        current_role_index = interaction.guild.roles.index(current_role)
+
+        async def check_role_time(chosen_role: discord.Role):
+            wanted_role_index = interaction.guild.roles.index(chosen_role)
+            difference = wanted_role_index - current_role_index
+            if difference < 0:
+                await interaction.followup.send(
+                    f"{member.mention} already have a role higher than {chosen_role.mention}"
+                )
+                return
+            if len(current_member.expected_exponential_intervals) > difference:
+                times = current_member.expected_exponential_intervals[:difference]
+                self.logger.info(f"User {times=}")
+                days_left = (
+                    (times[-1] - current_member.total_minutes_connected) / 60
+                ) / 24
+                await interaction.followup.send(
+                    f"The expected time for the user {member.mention} to achive {chosen_role.mention} is: {days_left} days",
+                )
+            else:
+                await interaction.followup.send(
+                    f"User {member.mention} does not have times to compare with"
+                )
+
+        if role is not None:
+            await check_role_time(role)
+        else:
+            if current_role_index == len(interaction.guild.roles) - 1:
+                await interaction.followup.send(
+                    f"User {member.mention} already have the highest role possible"
+                )
+                return
+            role = interaction.guild.roles[current_role_index + 1]
+            await check_role_time(role)
+
+    @control_subgroup.command(name="winning")
+    async def get_winning_member(self, interaction: discord.Interaction):
+        """Get the member with the highest time yet"""
+        self.logger.info(
+            f"Member {interaction.user.name} requested to know the highest member"
+        )
+        await interaction.response.defer()
+        interaction.channel.typing()
+        self.member_time_list.sort(
+            key=lambda x: x.total_minutes_connected, reverse=True
+        )
+        current_member = self.member_time_list[0]
+        member: discord.Member = interaction.guild.get_member(current_member.id)
+        if member is None:
+            await interaction.followup.send(
+                f"Sorry, I could not find the member with the ID {current_member.id}"
+            )
+        else:
+            await interaction.followup.send(
+                f"The member with the highest time until now is {member.mention} with {current_member.total_minutes_connected/60/24} days connected"
+            )
+
+    @control_subgroup.command(name="recalculate")
+    @commands.has_guild_permissions(administrator=True)
+    async def recalculate_time_intervals(
+        self,
+        interaction: discord.Interaction,
+        member: discord.Member,
+    ) -> None:
+        """Recalculate time intervals to gain the next roles"""
+        self.logger.info(
+            f"User {interaction.user.name} requested a recalculation of the intervals for member {member.name}"
+        )
+        try:
+            await interaction.response.defer()
+            interaction.channel.typing()
+
+            current_member, i = self.get_member_from_list(member.id)
+            if current_member is None:
+                await interaction.followup.send(
+                    f"User {member.mention} is not on the tracking list yet"
+                )
+                return
+
+            current_member.expected_exponential_intervals = (
+                self.calculate_exponential_interval(member)
+            )
+            self.member_time_list[i] = current_member
+            await interaction.followup.send(
+                f"Recalculated time intervals for the user {member.mention}. The new intervals are {current_member.expected_exponential_intervals} minutes"
+            )
+            self.save_to_json()
+        except MissingPermissions:
+            self.logger.error(
+                f"User {interaction.user.name} don't have the correct permissions to use this command"
+            )
+            if not interaction.response.is_done():
+
+                interaction.response.send_message(
+                    self.NO_PERMISSION_MSG, ephemeral=True
+                )
+            else:
+                interaction.followup.send(self.NO_PERMISSION_MSG, ephemeral=True)
+        except Exception as e:
+            self.logger.error(f"Error ocurred {e}")
+
+    @control_subgroup.command(name="promote")
+    @commands.has_guild_permissions(administrator=True)
+    async def promote_member(
+        self,
+        interaction: discord.Interaction,
+        member: discord.Member,
+    ) -> None:
+        """Promote a member to the next role and reset the timer tracker to the begining"""
+        self.logger.info(
+            f"Received promotion command for the user {member.name} by user {interaction.user.name}"
+        )
+        try:
+            await interaction.response.defer()
+            interaction.channel.typing()
+
+            if not interaction.user.guild_permissions.administrator:
+                await interaction.followup.send(
+                    f"{interaction.user.mention}, you are not an administrator."
+                )
+                return
+
+            m, _ = self.get_member_from_list(member.id)
+            if m is None:
+                await interaction.followup.send(
+                    f"Current {member.mention} is not in the track list, is not possible to interact with his rank",
+                    ephemeral=True,
+                )
+                self.logger.warning(
+                    f"Despite receiving the promote command the user {member.name} is not in the lis"
+                )
+                return
+
+            role = self.get_member_highest_role(member)
+            role_index = interaction.guild.roles.index(role)
+            if len(interaction.guild.roles) - 1 == role_index:
+                await interaction.followup.send(
+                    f"Current {member.mention} is already at the highest possible role.",
+                    ephemeral=True,
+                )
+            else:
+                next_role = interaction.guild.roles[role_index + 1]
+                await member.remove_roles(role)
+                await member.add_roles(next_role)
+                await interaction.followup.send(
+                    f"{member.mention} received a promotion to the role of {next_role.mention}"
+                )
+                current_member, i = self.get_member_from_list(member.id)
+                current_member.expected_exponential_intervals = (
+                    self.calculate_exponential_interval(member)
+                )
+                current_member.expected_exponential_intervals.pop(0)
+                self.member_time_list[i] = current_member
+                self.save_to_json()
+        except MissingPermissions:
+            self.logger.error(
+                f"User {interaction.user.name} don't have the correct permissions to use this command"
+            )
+            if not interaction.response.is_done():
+                interaction.response.send_message(
+                    self.NO_PERMISSION_MSG, ephemeral=True
+                )
+            else:
+                interaction.followup.send(self.NO_PERMISSION_MSG, ephemeral=True)
+        except Exception as e:
+            self.logger.error(f"Error ocurred {e}")
+
+    @control_subgroup.command(name="demote")
+    @commands.has_guild_permissions(administrator=True)
+    async def demote_member(
+        self,
+        interaction: discord.Interaction,
+        member: discord.Member,
+    ) -> None:
+        """Demote a member to the previous role and reset the timer tracker to the begining"""
+        self.logger.info(
+            f"Received demotion command for the user {member.name} by user {interaction.user.name}"
+        )
+        try:
+            await interaction.response.defer()
+            interaction.channel.typing()
+
+            if not interaction.user.guild_permissions.administrator:
+                await interaction.followup.send(
+                    f"{interaction.user.mention}, you are not an administrator."
+                )
+                return
+
+            m, _ = self.get_member_from_list(member.id)
+            if m is None:
+                await interaction.followup.send(
+                    f"Current {member.mention} is not in the track list, is not possible to interact with his rank",
+                    ephemeral=True,
+                )
+                self.logger.warning(
+                    f"Despite receiving the demote command the user {member.name} is not in the lis"
+                )
+                return
+
+            role = self.get_member_highest_role(member)
+            role_index = interaction.guild.roles.index(role)
+            if role_index - 1 > 0:
+                await interaction.followup.send(
+                    f"Current {member.mention} is already at the lowest possible role.",
+                    ephemeral=True,
+                )
+            else:
+                previous_role = interaction.guild.roles[role_index - 1]
+                await member.remove_roles(role)
+                await member.add_roles(previous_role)
+                await interaction.followup.send(
+                    f"{member.mention} received a demotion to the role of {previous_role.mention}"
+                )
+                current_member, i = self.get_member_from_list(member.id)
+                current_member.expected_exponential_intervals = (
+                    self.calculate_exponential_interval(member)
+                )
+                current_member.expected_exponential_intervals.pop(0)
+                self.member_time_list[i] = current_member
+                self.save_to_json()
+        except MissingPermissions:
+            self.logger.error(
+                f"User {interaction.user.name} don't have the correct permissions to use this command"
+            )
+            if not interaction.response.is_done():
+                interaction.response.send_message(
+                    self.NO_PERMISSION_MSG, ephemeral=True
+                )
+            else:
+                interaction.followup.send(self.NO_PERMISSION_MSG, ephemeral=True)
+        except Exception as e:
+            self.logger.error(f"Error ocurred {e}")
 
     @commands.Cog.listener()
     async def on_voice_state_update(
@@ -235,25 +493,24 @@ class Observer(commands.Cog, AppLogger):
 
         return current_member, self.member_time_list.index(current_member)
 
-    def update_user_role(self):
-        """Update user role base in the time"""
-        ...
-
     def calculate_exponential_interval(self, member: discord.Member):
         self.logger.info(f"Calculating exponential interval for {member.display_name}")
         starting_minutes_required = 200 * 60  # Hours * min = Total Min
+        # 2 years * days * hours * minutes = Total in minutes
+        total_minutes_to_max_role = 2 * 365 * 24 * 60
+
         roles = member.guild.roles
         # Split roles based on the current member role
         current_role = self.get_member_highest_role(member)
         index = roles.index(current_role)
         roles = roles[index:]
 
-        intervals = np.geomspace(1, 100, len(roles))
-        # Aplicar o aumento exponencial
-        for i in range(1, len(intervals)):
-            intervals[i] = intervals[i - 1] + starting_minutes_required
+        intervals = np.geomspace(
+            starting_minutes_required,
+            total_minutes_to_max_role,
+            len(roles),
+        )
         intervals_list = intervals.tolist()
-        intervals_list.pop(0)  # first is always 1.0, so we remove it
 
         return intervals_list
 
